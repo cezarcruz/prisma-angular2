5117e772cc24f879bf0e53dd328c8a1196ee77fb
Danielle <daniellesoldera@gmail.com>
2016-11-12 10:46:50 -0200

    insert angular material library

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,6 +11,7 @@ import { FormComponent } from './form/form.component';
 @NgModule({
     imports: [
         BrowserModule,
+        MaterialModule,
         FormsModule,
         RouterModule.forRoot([
             {
@@ -34,6 +36,5 @@ import { FormComponent } from './form/form.component';
     ],
     bootstrap: [ AppComponent ]
 })
-export class AppModule {
 
-}
+export class AppModule {}
